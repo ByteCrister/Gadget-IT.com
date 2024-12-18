a929b331d64e8ab499a46cc117433eb7d3d3aba9
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-12-18 19:00:51 +0600

    minor changes

--- server/config/auth.crypto.js
+++ server/config/auth.crypto.js
@@ -1,31 +1,28 @@
-// Load environment variables from .env file
 require('dotenv').config();
 
-// Import the crypto module
 const crypto = require('crypto');
 
-// Get the encryption key from the environment variables
+// *The encryption key
 const encryptionKey = process.env.CRYPTO_SECRET_KEY;
 
-// Ensure the encryption key is exactly 32 bytes (256 bits) for aes-256-cbc
+// *Ensure the encryption key is exactly 32 bytes (256 bits) for aes-256-cbc
 const key = Buffer.from(encryptionKey, 'utf8');
 
-// Pad or truncate the key to 32 bytes (256 bits)
-const paddedKey = Buffer.alloc(32);  // Allocate a 32-byte buffer
-key.copy(paddedKey);  // Copy the original key to the allocated buffer
+// *Pad or truncate the key to 32 bytes (256 bits)
+const paddedKey = Buffer.alloc(32);  
+key.copy(paddedKey);  
 if (key.length < 32) {
-    paddedKey.fill(0, key.length);  // Fill the remaining space with zeroes if the key is shorter than 32 bytes
+    paddedKey.fill(0, key.length); 
 }
 
-// Get the IV from the environment variables and convert it to a buffer
+// *The IV from the environment variables and convert it to a buffer
 const ivHex = process.env.CRYPTO_IV;
 const iv = Buffer.from(ivHex, 'hex');
 
-// Export the encryption and decryption functions
 module.exports = {
     //* Encrypting the bank source ID
     encryptBankSourceId: (sourceId) => {
-        // Create the cipher instance using the padded key and IV
+        // *Creating the cipher instance using the padded key and IV
         const cipher = crypto.createCipheriv('aes-256-cbc', paddedKey, iv);
         let encrypted = cipher.update(sourceId, 'utf8', 'hex');
         encrypted += cipher.final('hex');
@@ -34,7 +31,7 @@ module.exports = {
 
     //* Decrypting the bank source ID
     decryptBankSourceId: (encryptedData) => {
-        // Create the decipher instance using the padded key and IV
+        // *Creating the decipher instance using the padded key and IV
         const decipher = crypto.createDecipheriv('aes-256-cbc', paddedKey, iv);
         let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
         decrypted += decipher.final('utf8');
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT1 || process.env.PORT2;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on - http://localhost:${PORT}`);
