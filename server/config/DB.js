f46bbcc3aabf62802fd3183f0c1cd6c38f97e606
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-22 17:20:35 +0600

    minor change

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// const db = mysql.createConnection({
+//     host: process.env.DB_host,
+//     user: process.env.DB_user,
+//     password: process.env.DB_password,
+//     database: process.env.DB,
+//     port: process.env.PORT,
+//     connectTimeout: 10000
+// });
 const db = mysql.createConnection({
-    host: process.env.DB_host,
-    user: process.env.DB_user,
-    password: process.env.DB_password,
-    database: process.env.DB,
-    port: process.env.PORT,
+    host: 'sql.freedb.tech',
+    user: `freedb_shakib's db`,
+    password: '!aMy7wGuU8*V3A?',
+    database: 'freedb_gadget_it',
+    port: 3306,
     connectTimeout: 10000
 });
 db.connect((err) => {
