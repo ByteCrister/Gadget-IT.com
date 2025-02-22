9f6b7cc03fdd66c0dbbc1c8fab24771163d4a783
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-22 18:35:01 +0600

    minor change

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const productsControllers = require('../controllers/products.controllers');
 const productRouter = require('express').Router();
 
-productRouter.get(process.env.BACKEND_URL + '/products/category/menu/items', productsControllers.getProductCategories);
+productRouter.get('/products/category/menu/items', productsControllers.getProductCategories);
 productRouter.get('/products/categoryNames', productsControllers.getProductsCategoryNamesController);
 productRouter.get('/product_key_values/:mainCategory', productsControllers.getProductKeyValues);
 productRouter.get('/get/category_and_sub_category', productsControllers.getCategory);
