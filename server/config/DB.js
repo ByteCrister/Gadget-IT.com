656dc87a908b08bb8a8bcea262bc670d7a6ef6b2
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-22 17:30:13 +0600

    minor change

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_host,
-//     user: process.env.DB_user,
-//     password: process.env.DB_password,
-//     database: process.env.DB,
-//     port: process.env.PORT,
-//     connectTimeout: 10000
-// });
+const db = mysql.createConnection({
+    host: process.env.DB_host,
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    database: process.env.DB,
+    port: process.env.PORT,
+    connectTimeout: 10000
+});
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err.stack);
