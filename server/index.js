bef8035d083b8e9f6e552af095c52e22379c7832
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-22 17:08:47 +0600

    minor change

--- server/index.js
+++ server/index.js
@@ -4,5 +4,6 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
+    console.log("Database Config:", process.env.DB_HOST, process.env.DB_USER);
     console.log(`Server is running on - http://localhost:${PORT}`); // Log the port (for local development)
 });
