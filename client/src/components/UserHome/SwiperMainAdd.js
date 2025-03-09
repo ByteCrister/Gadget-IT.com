1c0ab4fc2baab3e5ed7d29bee355e18a4f80820d
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-03-09 23:48:30 +0600

    vercel change

--- client/src/components/UserHome/SwiperMainAdd.js
+++ client/src/components/UserHome/SwiperMainAdd.js
@@ -13,45 +13,48 @@ import SkeletonLoader from '../ui/SkeletonLoader';
 const SwiperMainAdd = () => {
     const { dataState } = useContext(useData);
 
-    if(!dataState.UserHomeContents?.advertisements || !dataState.UserHomeContents) return <SkeletonLoader />
-
     return (
         dataState.UserHomeContents?.advertisements?.length > 0 && (
             <section className={styles.SectionOne}>
-                <div className={styles.AddMainContainer}>
-                    <Swiper
-                        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-                        spaceBetween={0}
-                        slidesPerView={1}
-                        navigation
-                        pagination={{ clickable: true }}
-                        scrollbar={{ draggable: true }}
-                        autoplay={{ delay: 3000, disableOnInteraction: false }}
-                        loop={true}
-                        className={styles.mySwiper}
-                        key={uuidv4()}
-                    >
+                {
+                    (!dataState.UserHomeContents?.advertisements || !dataState?.UserHomeContents) ? <SkeletonLoader /> : <div className={styles.AddMainContainer}>
+                        <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                            spaceBetween={0}
+                            slidesPerView={1}
+                            navigation
+                            pagination={{ clickable: true }}
+                            scrollbar={{ draggable: true }}
+                            autoplay={{ delay: 3000, disableOnInteraction: false }}
+                            loop={true}
+                            className={styles.mySwiper}
+                            key={uuidv4()}
+                        >
+                            {
+                                dataState.UserHomeContents?.advertisements.filter((item) => {
+                                    return item.position === 'main'
+                                }).map((item, i) => {
+                                    return <SwiperSlide key={uuidv4()}>
+                                        <img key={uuidv4()} src={item.img} alt={`Advertisement-${i}`} />
+                                    </SwiperSlide>
+                                })
+                            }
+                        </Swiper>
+                    </div>
+                }
+
+                {
+                    (!dataState.UserHomeContents?.advertisements || !dataState?.UserHomeContents) ? <SkeletonLoader /> : <div className={styles.subCoverImg}>
                         {
                             dataState.UserHomeContents?.advertisements.filter((item) => {
-                                return item.position === 'main'
-                            }).map((item, i) => {
-                                return <SwiperSlide key={uuidv4()}>
-                                    <img key={uuidv4()} src={item.img} alt={`Advertisement-${i}`} />
-                                </SwiperSlide>
+                                return item.position === 'sub'
+                            }).slice(0, 2).map((item, i) => {
+                                return <img key={uuidv4()} src={item.img} alt={`Advertisement-${i}`} />
                             })
                         }
-                    </Swiper>
-                </div>
+                    </div>
+                }
 
-                <div className={styles.subCoverImg}>
-                    {
-                        dataState.UserHomeContents?.advertisements.filter((item) => {
-                            return item.position === 'sub'
-                        }).slice(0, 2).map((item, i) => {
-                            return <img key={uuidv4()} src={item.img} alt={`Advertisement-${i}`} />
-                        })
-                    }
-                </div>
             </section>
         )
     );
